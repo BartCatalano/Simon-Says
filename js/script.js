d4fa0d42a5ad8fa911a4c8eff06d995084d73e6d
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-11 15:54:44 +0100

    feat: generatore numeri, contatore tempo e tasto di start collegati tra loro e visibili in pagina. creazione pagina input numerici (wip)

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
+// IMPORTO DA HTML
+const startBtn = document.getElementById("start-btn");
+const listaNumeri = document.getElementById("lista-Numeri");
+const contatoretempo = document.getElementById("contatore-tempo");
+
+
+
 
 // GENERAZIONE NUMERI RANDOM
 // creo un array per inserire i numeri
+// CREO EVENTO DI GIOCO CON PULSANTE CHE AVVIA IL CONTEGGIO
+// INSERISCO TUTTO NEL CLICK DEL BOTONE
 const arrayNumeri = [];
-
-for (let i = 0; i < 5; i++) {
-    const numeroRandom = Math.floor(Math.random() * 100); // Numeri casuali tra 0 e 99
-    arrayNumeri.push(numeroRandom + 1);
-    
-  }
-  
-
-//   CREO UN CONTO DA 30' A 0'
 let contatore = 30;
-const conteggio = setInterval(function(){
-console.log(contatore);
-contatore--;
-if (contatore === 0) {
-    clearInterval(conteggio);
-}
+startBtn.addEventListener("click", function() {
+    for (let i = 0; i < 5; i++) {
+        const numeroRandom = Math.floor(Math.random() * 100); // Numeri casuali tra 0 e 99
+        arrayNumeri.push(numeroRandom + 1);
+        listaNumeri.innerHTML = arrayNumeri;
+        
+        
+      }
+    const conteggio = setInterval(function(){
+        console.log(contatore);
+        contatore--;
+        contatoretempo.innerHTML = contatore;
+        if (contatore === 0) {
+            clearInterval(conteggio);
+        }
+        
+        },1000);
+    
+    
+});
 
-},100);
+
+
+
+
+
+
 
 
   
